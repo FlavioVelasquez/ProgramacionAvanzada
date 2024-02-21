e207d9efdd9304463d18e2174f3c740921a4156a
FlavioVelasquez <22000390@galileo.edu>
2024-02-20 22:53:00 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var Mousetrap = require('mousetrap');
+
+var botonUno = document.getElementById('botonUno')
+var botonDos = document.getElementById('botonDos')
+var botonSuma = document.getElementById('botonSuma')
+var actual = document.getElementById('actual')
+var resultado = document.getElementById('resultado')
+
+botonUno.addEventListener('click', clickUno)
+botonDos.addEventListener('click', clickDos)
+botonSuma.addEventListener('click', clickSuma)
+
+Mousetrap.bind('1', clickUno)
+
+function clickUno(){
+    actual.innerHTML+='1'
+}
+
+function clickDos(){
+    actual.innerHTML+='2'
+}
+
+function clickSuma(){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+
+    resultado.innerHTML = valorActual + valorResultado
+    actual.innerHTML = '0'
+
+}
